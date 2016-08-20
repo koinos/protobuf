2695e4469dcb4ffa2ed38c43b0e4b62bbf86063a
Fabian Winnen <fabian.winnen@kinemic.de>
2016-08-20 14:10:05 +0200

    Introduced partial builds with options: * protobuf_BUILD_PROTOC * protobuf_BUILD_LIBPROTOBUF * protobuf_BUILD_LIBPROTOBUF_LITE

--- cmake/protobuf-options.cmake
+++ cmake/protobuf-options.cmake
@@ -4,4 +4,5 @@ mark_as_advanced(protobuf_VERBOSE)
 
 # FindProtobuf module compatibel
 option(protobuf_MODULE_COMPATIBLE "CMake build-in FindProtobuf.cmake module compatible" OFF)
+
 mark_as_advanced(protobuf_MODULE_COMPATIBLE)
