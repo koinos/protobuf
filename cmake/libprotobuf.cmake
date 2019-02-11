b932f957ca0eec093f012360d01a79207bb9a9bb
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-02-11 17:37:07 +0300

    CMake: Android log library dependency

--- cmake/libprotobuf.cmake
+++ cmake/libprotobuf.cmake
@@ -114,9 +114,9 @@ endif()
 
 add_library(libprotobuf ${protobuf_SHARED_OR_STATIC}
   ${libprotobuf_lite_files} ${libprotobuf_files} ${libprotobuf_includes} ${libprotobuf_rc_files})
-target_link_libraries(libprotobuf ${CMAKE_THREAD_LIBS_INIT})
+target_link_libraries(libprotobuf PUBLIC ${CMAKE_THREAD_LIBS_INIT})
 if(protobuf_WITH_ZLIB)
-  target_link_libraries(libprotobuf ${ZLIB_LIBRARIES})
+  target_link_libraries(libprotobuf PUBLIC ${ZLIB_LIBRARIES})
 endif()
 if(protobuf_LINK_LIBATOMIC)
   target_link_libraries(libprotobuf atomic)
@@ -135,3 +135,8 @@ set_target_properties(libprotobuf PROPERTIES
     OUTPUT_NAME ${LIB_PREFIX}protobuf
     DEBUG_POSTFIX "${protobuf_DEBUG_POSTFIX}")
 add_library(protobuf::libprotobuf ALIAS libprotobuf)
+
+if(ANDROID)
+  find_package(android_log REQUIRED)
+  target_link_libraries(libprotobuf PUBLIC android_log::android_log)
+endif()
